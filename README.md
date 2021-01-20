314897f6a47b38273ae9c345696165a52ec50ca5
Andy Edwards <jedwards@fastmail.com>
2021-01-19 20:41:45 -0600

    feat: initial commit

--- README.md
+++ README.md
@@ -1,30 +1,42 @@
-# untranspiled-js-library-skeleton
+# @jcoreio/ec2-ssh
 
-[![CircleCI](https://circleci.com/gh/jedwards1211/untranspiled-js-library-skeleton.svg?style=svg)](https://circleci.com/gh/jedwards1211/untranspiled-js-library-skeleton)
-[![Coverage Status](https://codecov.io/gh/jedwards1211/untranspiled-js-library-skeleton/branch/master/graph/badge.svg)](https://codecov.io/gh/jedwards1211/untranspiled-js-library-skeleton)
+[![CircleCI](https://circleci.com/gh/jcoreio/ec2-ssh.svg?style=svg)](https://circleci.com/gh/jcoreio/ec2-ssh)
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
 [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
-[![npm version](https://badge.fury.io/js/untranspiled-js-library-skeleton.svg)](https://badge.fury.io/js/untranspiled-js-library-skeleton)
+[![npm version](https://badge.fury.io/js/%40jcoreio%2Fec2-ssh.svg)](https://badge.fury.io/js/%40jcoreio%2Fec2-ssh)
 
-This is my personal skeleton for creating an untranspiled JS library npm package. You are welcome to use it.
-
-## Quick start
-
-```sh
-npx 0-60 clone https://github.com/jedwards1211/untranspiled-js-library-skeleton.git
+```
+npm i -g ec2-ssh
+# Or
+npx ec2-ssh
 ```
 
-## Tools used
+Use at your own risk. Prompts you to select an EC2 instance, then SSHes into that instance, attempting to select the correct username automatically, and using the internal DNS hostname, and the identity file at `~/.ssh/<KeyName>.pem` if it exists, where `KeyName` is the property of the EC2 instance in the `aws-sdk` response.
 
-- mocha
-- chai
-- istanbul
-- nyc
-- eslint
-- eslint-watch
-- flow
-- flow-watch
-- husky
-- semantic-release
-- Travis CI
-- Coveralls
+If you're not storing your identity files in that manner, you can add the following to your `~/.ssh/config`:
+
+```
+Host *.compute.internal
+  User ec2-user
+  IdentityFile ~/.ssh/your-identity-file.pem
+```
+
+It will set the `AWS_SDK_LOAD_CONFIG` environment variable to load the default region from your `~/.aws/config`.
+See https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-region.html for other ways
+of setting the default region.
+
+```
+> ec2-ssh
+? Select an EC2 Instance (region: us-west-2) ›
+    i-00000000000000000 foo (recent)
+    i-00000000000000001 bar
+❯   i-00000000000000002 baz
+    i-00000000000000003 qux
+ssh -t ubuntu@ip-192-168-0-1.us-west-2.compute.internal
+Welcome to Ubuntu 18.04.1 LTS (GNU/Linux 4.15.0-1021-aws x86_64)
+
+ * Documentation:  https://help.ubuntu.com
+ * Management:     https://landscape.canonical.com
+ * Support:        https://ubuntu.com/advantage
+...
+```
---
+++ index.js
@@ -0,0 +1,111 @@
+#!/usr/bin/env node
+
+'use strict'
+
+if (process.env.AWS_SDK_LOAD_CONFIG == null)
+  process.env.AWS_SDK_LOAD_CONFIG = '1'
+
+const AWS = require('aws-sdk')
+const { selectEC2Instance } = require('@jcoreio/aws-select-cli-prompts')
+const { spawn } = require('child_process')
+const os = require('os')
+const fs = require('fs')
+const path = require('path')
+const { promisify } = require('util')
+
+async function getAmiName(ec2, ImageId) {
+  const { Images } = await ec2.describeImages({ ImageIds: [ImageId] }).promise()
+  const name = Images && Images[0] && Images[0].Name
+  if (!name) throw new Error(`failed to get name for image: ${ImageId}`)
+  return name
+}
+
+function getUser(ami) {
+  if (/ubuntu|nanostack/i.test(ami)) return 'ubuntu'
+  if (/debian/i.test(ami)) return 'admin'
+  if (/fedora/i.test(ami)) return 'fedora'
+  if (/centos/i.test(ami)) return 'centos'
+  if (/bitnami/i.test(ami)) return 'bitnami'
+  if (/turnkey|omni/i.test(ami)) return 'root'
+  return 'ec2-user'
+}
+
+function signalCode(signal) {
+  switch (signal) {
+    case 'SIGABRT':
+      return 6
+    case 'SIGHUP':
+      return 1
+    case 'SIGILL':
+      return 4
+    case 'SIGINT':
+      return 2
+    case 'SIGKILL':
+      return 9
+    case 'SIGPIPE':
+      return 13
+    case 'SIGQUIT':
+      return 3
+    case 'SIGSEGV':
+      return 11
+    case 'SIGTERM':
+      return 15
+    case 'SIGTRAP':
+      return 5
+  }
+  return 0
+}
+
+async function ec2ssh({ ec2 = new AWS.EC2() } = {}) {
+  const instance = await selectEC2Instance({ ec2 })
+
+  const { ImageId, KeyName, PrivateDnsName, PublicDnsName } = instance
+  let host = PrivateDnsName || PublicDnsName
+  if (!host)
+    throw new Error(`instance doesn't have a PrivateDnsName or PublicDnsName`)
+
+  const user = ImageId ? getUser(await getAmiName(ec2, ImageId)) : null
+
+  if (user) host = `${user}@${host}`
+
+  const args = ['-t']
+  if (KeyName) {
+    const identityFile = path.join(os.homedir(), '.ssh', `${KeyName}.pem`)
+    try {
+      await promisify(fs.stat)(identityFile)
+      args.push('-i', identityFile)
+    } catch (error) {
+      // ignore
+    }
+  }
+
+  args.push(host, ...process.argv.slice(2))
+
+  // eslint-disable-next-line no-console
+  console.log('ssh', ...args)
+
+  const child = spawn('ssh', args, { stdio: 'inherit' })
+
+  return await new Promise((resolve, reject) => {
+    child.once('error', reject)
+    child.once('close', (code, signal) => {
+      resolve({ code, signal })
+      if (typeof code === 'number') process.exit(code)
+      process.exit(signalCode(signal) + 127)
+    })
+  })
+}
+
+if (require.main === module) {
+  ec2ssh().then(
+    ({ code, signal }) => {
+      if (typeof code === 'number') process.exit(code)
+      process.exit(signalCode(signal) + 127)
+    },
+    error => {
+      // eslint-disable-next-line no-console
+      console.error(error.message)
+      process.exit(1)
+    }
+  )
+}
